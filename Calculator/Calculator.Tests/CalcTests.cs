6c4888b91b41be68f3d0f5d8df452f65014d3c41
André <andrerrr@outlook.de>
2019-02-20 14:12:49 +0100

    Tests sind cool

---
+++ Calculator/Calculator.Tests/CalcTests.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.QualityTools.Testing.Fakes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator.Tests
+{
+    [TestClass]
+    public class CalcTests
+    {
+        [TestMethod]
+        public void Calc_Sum_4_and_5_results_9()
+        {
+            //Arrange
+            var calc = new Calc();
+
+            //Act   
+            var result = calc.Sum(4, 5);
+
+            //Assert                         
+            Assert.AreEqual(9, result);
+        }
+
+        [TestMethod]
+        public void Calc_Sum_0_and_0_results_0()
+        {
+            //Arrange
+            var calc = new Calc();
+
+            //Act   
+            var result = calc.Sum(0, 0);
+
+            //Assert                         
+            Assert.AreEqual(0, result);
+        }
+
+
+        [TestMethod]
+        public void Calc_Sum_MAX_and_1_throws_OverflowException()
+        {
+            var calc = new Calc();
+
+            Assert.ThrowsException<OverflowException>(() => calc.Sum(int.MaxValue, 1));
+
+        }
+
+        [TestMethod]
+        [DataRow(2, 3, 5)]
+        [DataRow(-2, 3, 1)]
+        [DataRow(-2, -3, -5)]
+        public void Calc_Sum(int a, int b, int soll)
+        {
+            //Arrange
+            var calc = new Calc();
+
+            //Act   
+            var result = calc.Sum(a, b);
+
+            //Assert                         
+            Assert.AreEqual(soll, result);
+        }
+
+        [TestMethod]
+        public void Calc_IsWeekend()
+        {
+            var calc = new Calc();
+
+            using (ShimsContext.Create())
+            {
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2019, 2, 18);
+                Assert.IsFalse(calc.IsWeekend());
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2019, 2, 19);
+                Assert.IsFalse(calc.IsWeekend());
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2019, 2, 20);
+                Assert.IsFalse(calc.IsWeekend());
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2019, 2, 21);
+                Assert.IsFalse(calc.IsWeekend());
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2019, 2, 22);
+                Assert.IsFalse(calc.IsWeekend());
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2019, 2, 23);
+                Assert.IsTrue(calc.IsWeekend());
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2019, 2, 24);
+                Assert.IsTrue(calc.IsWeekend());
+            }
+        }
+    }
+}
---
+++ Calculator/Calculator.Tests/HalloDelegate.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Tests
+{
+    delegate void EinfacherDelegate();
+    delegate void DelegateMitParameter(string txt);
+    delegate long CalcDelegate(int a, int b);
+
+    class HalloDelegate
+    {
+        public HalloDelegate()
+        {
+            EinfacherDelegate dings = EinfacheMethode;
+            Action dingsAlsAction = EinfacheMethode;
+            Action dingsAlsAction2 = delegate () { Console.WriteLine("Hallo"); };
+            Action dingsAlsAction3 = () => { Console.WriteLine("Hallo"); };
+            Action dingsAlsAction4 = () => Console.WriteLine("Hallo");
+
+
+            DelegateMitParameter deleMP = ZeigeText;
+            Action<string> deleMPAction = ZeigeText;
+            Action<string> deleMPAction2 = (string x) => Console.WriteLine(x);
+            DelegateMitParameter deleMPAction3 = (x) => Console.WriteLine(x);
+            Action<string> deleMPAction4 = x => Console.WriteLine(x);
+            Action<string> deleMPAction5 = Console.WriteLine;
+
+            CalcDelegate calcDele = Sum;
+            Func<int, int, long> calcDeleFunc = Sum;
+            CalcDelegate calcDele2 = (int x, int y) =>
+            {
+                return x + y;
+            };
+
+            CalcDelegate calcDele3 = (x, y) => x + y;
+
+            var texte = new List<string>();
+            var allesWasMitBAnfängt = texte.Where(x => x.StartsWith("b"));
+            var allesWasMitBAnfängt22 = texte.Where(Filter);
+
+        }
+
+        private bool Filter(string arg)
+        {
+            if (arg.StartsWith("b"))
+                return true;
+            else
+                return false;
+        }
+
+        private long Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        private void ZeigeText(string txt)
+        {
+            Console.WriteLine(txt);
+        }
+
+        public void EinfacheMethode()
+        {
+            Console.WriteLine("Hallo");
+        }
+    }
+}
