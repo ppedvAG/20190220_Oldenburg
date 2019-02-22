135c6377514e0e12b10dee9bda0609f98670fc9c
André <andrerrr@outlook.de>
2019-02-22 09:04:46 +0100

    Async/Await

---
+++ HalloAsync/HalloAsync/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace HalloAsync
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(107, 53);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(179, 27);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Starten (ohne Threading)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(106, 243);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(286, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button2.Location = new System.Drawing.Point(107, 99);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(190, 27);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Start Task (MethodInvoker)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.AutoSize = true;
+            this.button3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button3.Location = new System.Drawing.Point(107, 132);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(192, 27);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Start Task (TaskScheduler)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.AutoSize = true;
+            this.button4.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button4.Location = new System.Drawing.Point(305, 132);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(87, 27);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Abbrechen";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.AutoSize = true;
+            this.button5.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button5.Location = new System.Drawing.Point(107, 190);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(127, 27);
+            this.button5.TabIndex = 5;
+            this.button5.Text = "Start Async/Await";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.AutoSize = true;
+            this.button6.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.button6.Location = new System.Drawing.Point(336, 190);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(123, 27);
+            this.button6.TabIndex = 6;
+            this.button6.Text = "MeinAsync/Await";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(523, 320);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ HalloAsync/HalloAsync/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HalloAsync
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                progressBar1.Value = i;
+                Text = $"{i}";
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            Task.Run(() =>
+            {
+                for (int i = 0; i <= 100; i++)
+                {
+                    progressBar1.Invoke((MethodInvoker)delegate { progressBar1.Value = i; });
+
+                    Thread.Sleep(100);
+                }
+                progressBar1.Invoke((MethodInvoker)delegate { button2.Enabled = !false; });
+            });
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var ts = TaskScheduler.FromCurrentSynchronizationContext();
+            cts = new CancellationTokenSource();
+            button3.Enabled = false;
+            Task.Run(() =>
+            {
+                for (int i = 0; i <= 100; i++)
+                {
+                    Thread.Sleep(10);
+                    int z = i;
+                    Task.Factory.StartNew(() => progressBar1.Value = z,
+                                            cts.Token,
+                                            TaskCreationOptions.None,
+                                            ts);
+
+                    if (cts.IsCancellationRequested)
+                    {
+                        //cleaniup
+                        break;
+                    }
+                }
+            }, cts.Token).ContinueWith(t => button3.Enabled = !false, CancellationToken.None, TaskContinuationOptions.None, ts)
+                         .ContinueWith(t => MessageBox.Show("Fertig"), cts.Token, TaskContinuationOptions.None, ts);
+        }
+
+        CancellationTokenSource cts;
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            cts?.Cancel();
+
+        }
+
+        private async void button5_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                progressBar1.Value = i;
+                await Task.Delay(100);
+            }
+        }
+
+
+        private long AlteLangsameFunction(int zahl)
+        {
+            Thread.Sleep(2000);
+            return zahl * 2;
+        }
+
+        private Task<long> AlteLangsameFunctionAsync(int zahl)
+        {
+            return Task.Run<long>(() => AlteLangsameFunction(zahl));
+        }
+
+
+        private async void button6_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show((await AlteLangsameFunctionAsync(48)).ToString());
+        }
+    }
+}
